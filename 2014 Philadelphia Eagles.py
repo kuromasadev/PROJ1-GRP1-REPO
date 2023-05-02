c102e9b49454584c00d3064f813c46a7a71116cc
Ashleymtoliver20 <128104128+Ashleymtoliver20@users.noreply.github.com>
2023-05-01 20:33:00 -0500

    Add files via upload

---
+++ 2014 Philadelphia Eagles.py
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>2014 Philadelphia Eagles</title>
+<script language="JavaScript" type="text/javascript">
+<!-- 
+document._domino_target = "_self";
+function _doClick(v, o, t) {
+  var returnValue = false;
+  var url="/football/pro/results.nsf/Teams/2014-phi?OpenDocument&Click=" + v;
+  if (o.href != null) {
+    o.href = url;
+    returnValue = true;
+  } else {
+    if (t == null)
+      t = document._domino_target;
+    window.open(url, t);
+  }
+  return returnValue;
+}
+// -->
+</script>
+</head>
+<body text="#000000" bgcolor="#FFFFFF">
+
+<form action="">
+<table CELLPADDING=8 width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr valign="top"><td width="13%" bgcolor="#E0F1FF"><div align="center"><b><BR>Site Pages</b><br>
+<a href="/football"><b><u><font color="#0000FF">Home</font></u></b></a><br>
+<a href="/football/site-contents.html"><b><u><font color="#0000FF">Contents</font></u></b></a><a href="/football/site-index.html"></a><br>
+<a href="/football/site-index.html"><b><u><font color="#0000FF">Index</font></u></b></a><br>
+<br>
+<hr width="95%" size="5" align="center" noshade style="color:#000080; "><br>
+<a href="/football/pro/results.nsf"><b><u>Games</u></b></a><br>
+<br>
+<hr width="95%" size="5" align="center" noshade style="color:#000080; "><br>
+<b>Eagles</b><br>
+<b><A HREF="/football/pro/teams.nsf/histories/eagles">Historical<BR><A HREF="/football/pro/stats.nsf/Annual/2014-phi">2014 Stats</A><BR><A HREF="/football/pro/rosters.nsf/Annual/2014-phi">2014 Roster</A></b><br>
+<br>
+<hr width="95%" size="5" align="center" noshade style="color:#000080; "><br>
+<b>2014</b><br>
+<b><A HREF="/football/pro/standings.nsf/Seasons/2014">Standings</A><BR><A HREF="/football/pro/stats.nsf/By/Season?OpenDocument&Season=2014">Stats</A><BR><A HREF="/football/pro/rosters.nsf/By/Season?OpenDocument&Season=2014">Rosters</A><BR><A HREF="/football/pro/results.nsf/By/Season?OpenDocument&Season=2014">Games</A></b><br>
+<b><BR></b></div></td><td width="87%"><b><i><font size="6">2014 Philadelphia Eagles</font></i></b><br>
+<br>
+<b><font size="4">Regular Season</font></b><br>
+<TABLE CELLPADDING="3"><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-01">Week 1</A></TD> <TD></TD> <TD>Sep 7</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-01-jac-phi">34-17</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-jac">Jacksonville Jaguars</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-02">Week 2</A></TD> <TD></TD> <TD>Sep 15</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-02-phi-ind">30-27</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-ind">Indianapolis Colts</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-03">Week 3</A></TD> <TD></TD> <TD>Sep 21</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-03-was-phi">37-34</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-was">Washington Redskins</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-04">Week 4</A></TD> <TD></TD> <TD>Sep 28</TD> <TD>L</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-04-phi-sf">26-21</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-sf">San Francisco 49ers</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-05">Week 5</A></TD> <TD></TD> <TD>Oct 5</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-05-stl-phi">34-28</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-stl">St. Louis Rams</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-06">Week 6</A></TD> <TD></TD> <TD>Oct 12</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-06-nyg-phi">27-0</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-nyg">New York Giants</A> </TD></TR><TR ALIGN="center"> <TD><A HREF="/football/pro/results.nsf/Weeks/2014-07">Week 7</A></TD> <TD></TD> <TD COLSPAN=3>Bye Week</TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-08">Week 8</A></TD> <TD></TD> <TD>Oct 26</TD> <TD>L</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-08-phi-arz">24-20</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-arz">Arizona Cardinals</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-09">Week 9</A></TD> <TD></TD> <TD>Nov 2</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-09-phi-hou">31-21</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-hou">Houston Texans</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-10">Week 10</A></TD> <TD></TD> <TD>Nov 10</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-10-car-phi">45-21</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-car">Carolina Panthers</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-11">Week 11</A></TD> <TD></TD> <TD>Nov 16</TD> <TD>L</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-11-phi-gb">53-20</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-gb">Green Bay Packers</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-12">Week 12</A></TD> <TD></TD> <TD>Nov 23</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-12-ten-phi">43-24</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-ten">Tennessee Titans</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-13">Week 13</A></TD> <TD></TD> <TD>Nov 27</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-13-phi-dal">33-10</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-dal">Dallas Cowboys</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-14">Week 14</A></TD> <TD></TD> <TD>Dec 7</TD> <TD>L</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-14-sea-phi">24-14</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-sea">Seattle Seahawks</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-15">Week 15</A></TD> <TD></TD> <TD>Dec 14</TD> <TD>L</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-15-dal-phi">38-27</A></TD> <TD ALIGN="left">vs <A HREF="/football/pro/results.nsf/Teams/2014-dal">Dallas Cowboys</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-16">Week 16</A></TD> <TD></TD> <TD>Dec 20</TD> <TD>L</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-16-phi-was">27-24</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-was">Washington Redskins</A> </TD></TR><TR ALIGN="center"><TD> <A HREF="/football/pro/results.nsf/Weeks/2014-17">Week 17</A></TD> <TD></TD> <TD>Dec 28</TD> <TD>W</TD> <TD> <A HREF="/football/boxes/index.nsf/Games/2014-17-phi-nyg">34-26</A></TD> <TD ALIGN="left">at <A HREF="/football/pro/results.nsf/Teams/2014-nyg">New York Giants</A> </TD></TR></TABLE></td></tr>
+</table>
+<br>
+<hr width="100%" size="5" align="left" noshade style="color:#000000; "><br>
+<i>Generated: </i><i>04/21/2018</i><br>
+<i>Football Site Owner: </i><a href="mailto:jtroan@jt-sw.com"><i>John Troan</i></a><br>
+</form>
+</body>
+</html>
